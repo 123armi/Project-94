09ddf431dfed7ed8af008d6841bcc4f6f6be4246
123armi <75014697+123armi@users.noreply.github.com>
2021-06-01 23:12:45 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCxBtRRyZb7dXdGIUqKyMzlSqY5HcTn2FI",
+    authDomain: "kwitter-d5ad1.firebaseapp.com",
+    databaseURL: "https://kwitter-d5ad1-default-rtdb.firebaseio.com",
+    projectId: "kwitter-d5ad1",
+    storageBucket: "kwitter-d5ad1.appspot.com",
+    messagingSenderId: "334646613478",
+    appId: "1:334646613478:web:b1ba348c004ff305e0b227"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
